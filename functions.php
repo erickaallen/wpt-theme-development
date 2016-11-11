762b75bf04f3e6eec915050c175321366ae98bcc
Ericka Allen <ericka.allen@gmail.com>
2016-11-10 21:52:32 -0500

    Splitting content between header/footer and index files

--- functions.php
+++ functions.php
@@ -2,8 +2,8 @@
 
 function wpt_theme_styles() {
 
-    wp_enqueue_style( 'foundation_css', get_template_directory_uri() . '/css/foundations.css' );
-    wp_enqueue_style( 'normalize_css', get_template_directory_uri() . '/css/normalize.css' );
+    wp_enqueue_style( 'foundation_css', get_template_directory_uri() . '/css/foundation.css' );
+    // wp_enqueue_style( 'normalize_css', get_template_directory_uri() . '/css/normalize.css' );
     wp_enqueue_style( 'googleFont_css', 'http://fonts.googleapis.com/css?family=Asap:400,700,400italic,700italic' );
     wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
 
