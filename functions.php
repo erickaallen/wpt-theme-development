2c7c1c6f4abd3f73159894d781e1751083148824
Ericka Allen <ericka.allen@gmail.com>
2016-11-21 18:07:44 -0500

    Adding theme support for menus

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php
 
+add_theme_support( 'menus' );
+
 function wpt_theme_styles() {
 
     wp_enqueue_style( 'foundation_css', get_template_directory_uri() . '/css/foundation.css' );
@@ -12,7 +14,7 @@ add_action( 'wp_enqueue_scripts', 'wpt_theme_styles' );
 
 function wpt_theme_js() {
     wp_enqueue_script( 'modernizer_js', get_template_directory_uri() . '/js/modernizer.js', '', '', false );
-    wp_enqueue_script( 'foundation_js', get_template_directory_uri() . '/js/foundation.js', array('jquery'), '', true );
+    wp_enqueue_script( 'foundation_js', get_template_directory_uri() . '/js/foundation.min.js', array('jquery'), '', true );
     wp_enqueue_script( 'main_js', get_template_directory_uri() . '/js/app.js', array('jquery', 'foundation_js'), '', true );
 }
 add_action( 'wp_enqueue_scripts', 'wpt_theme_js' );
