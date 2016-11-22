12689a4ab75443682884d2b2a09bb939c4226be5
Ericka Allen <ericka.allen@gmail.com>
2016-11-22 12:39:07 -0500

    Registering primary nav menu

--- functions.php
+++ functions.php
@@ -2,6 +2,14 @@
 
 add_theme_support( 'menus' );
 
+function register_theme_menus() {
+
+    register_nav_menus(
+        array(
+            'primary-menu' => __( 'Primary Menu' )
+        )
+    );
+}
 function wpt_theme_styles() {
 
     wp_enqueue_style( 'foundation_css', get_template_directory_uri() . '/css/foundation.css' );
